095a9da1b77ab7533730d5f6cba84e7f0a1e229f
Omar Chadidi <omarchadidi@gmail.com>
2022-09-22 22:47:30 +0100

    Add: add testing with jest & change setup from babel to ts-jest

---
+++ __test__/index.spec.ts
@@ -0,0 +1,62 @@
+require("isomorphic-fetch");
+import Reqeza from "../src/index";
+
+const BASE_URL = "http://localhost:3001";
+
+jest.mock("isomorphic-fetch");
+
+describe("Creating new instance of http.", () => {
+  it("Should have all HTTP request methods", async () => {
+    const http = Reqeza.create({
+      PREFIX_URL: {
+        API: BASE_URL,
+      },
+    });
+
+    const METHODS = [
+      "get",
+      "head",
+      "put",
+      "delete",
+      "post",
+      "patch",
+      "options",
+    ];
+
+    METHODS.forEach(async (prop) => {
+      expect(typeof http[prop]).toBe("function");
+      expect(http).toHaveProperty(prop);
+    });
+  });
+
+  it("Methods should return setType methods.", async () => {
+    const http = Reqeza.create({
+      PREFIX_URL: {
+        API: BASE_URL,
+      },
+    });
+
+    const TYPES_METHODS = ["json", "text", "blob", "arrayBuffer", "formData"];
+
+    TYPES_METHODS.forEach(async (prop) => {
+      expect(http.get("/hello")).toHaveProperty(prop);
+    });
+  });
+
+  it("Should return error with invalid url.", async () => {
+    const http = Reqeza.create();
+
+    async function fetch() {
+      try {
+        const { data } = await http.get("fakestoreapi.com/products").json();
+        return data;
+      } catch (error) {
+        return error;
+      }
+    }
+
+    const res = await fetch();
+
+    expect(res.toJSON().error.message).toBe("The given URI is invalid.");
+  });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,13 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
   // An array of file extensions your modules use
-  moduleFileExtensions: ["js", "json", "ts"],
+  moduleFileExtensions: ["js", "ts"],
 };
+
+export default config;
